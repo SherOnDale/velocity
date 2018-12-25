570c6f108e1f8e4c5a4fc2a51a87f9103a6b4ee8
Sherin Binu <sherinbinu@hotmail.com>
2018-12-26 01:51:56 +0530

    change page metdata

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-const pkg = require('./package');
-
 module.exports = {
   mode: 'universal',
 
@@ -7,11 +5,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Velocity Events',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Welcome to Velocity Events'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
